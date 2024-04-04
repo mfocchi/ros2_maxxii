f4d21713a3ed46f2b546eb815eeea75860a0f536
Davide Dorigoni <ddorigoni@fbk.eu>
2024-04-04 16:55:41 +0200

    add unit tests for encoder and motor

---
+++ test/encoder_unittest.cpp
@@ -0,0 +1,21 @@
+#include "ros2_maxxii/encoder.h"
+#include <gtest/gtest.h>
+
+// Test for Encoder() constructor
+TEST(EncoderTests, Constructor)
+{
+    Encoder encoder(200.0);
+    EXPECT_EQ(encoder.getPPR(), 200);
+
+    EXPECT_ANY_THROW(Encoder encoder(0.0));
+}
+
+// Test for setReferencePulseCount() method
+TEST(EncoderTests, SetReferencePulseCount) 
+{
+    Encoder encoder(100.0);
+    encoder.setReferencePulseCount(100);
+    EXPECT_EQ(encoder.getPulseCount(), -100);
+    encoder.setReferencePulseCount(0.0);
+    EXPECT_EQ(encoder.getPulseCount(), 0.0);
+}
---
+++ test/motor_unittest.cpp
@@ -0,0 +1,103 @@
+#include "ros2_maxxii/motor.h"
+#include <gtest/gtest.h>
+
+
+// Define some example values for testing
+constexpr double MAX_RPM = 5000.0;
+constexpr double TORQUE_CONSTANT = 0.002;
+constexpr double MAX_AMP = 1.0;
+
+TEST(MotorTest, UnitConversion) {
+    double rpm = convert_vel_from_rpm(100.0, RPM);
+    double radps = convert_vel_from_rpm(100.0, RADPS);
+    double degps = convert_vel_from_rpm(100.0, DEGPS);
+    EXPECT_EQ(rpm, 100.0);
+    EXPECT_NEAR(radps, 10.4719755, 0.000001);
+    EXPECT_NEAR(degps, 600.0, 0.000001);
+
+    rpm = convert_vel_from_rpm(convert_vel_into_rpm(100.0, RPM), RPM);
+    radps = convert_vel_from_rpm(convert_vel_into_rpm(100.0, RADPS), RADPS);
+    degps = convert_vel_from_rpm(convert_vel_into_rpm(100.0, DEGPS), DEGPS);
+    EXPECT_NEAR(rpm, 100.0, 0.000001);
+    EXPECT_NEAR(radps, 100.0, 0.000001);
+    EXPECT_NEAR(degps, 100.0, 0.000001);
+}
+
+// Test case for constructor with mode, torque_constant, and max_rpm
+TEST(MotorTest, ConstructorWithParameters) {
+    Motor motor(VELOCITY, TORQUE_CONSTANT, MAX_RPM, MAX_AMP);
+
+    EXPECT_EQ(motor.getActualVelocity(RPM), 0.0);
+    EXPECT_EQ(motor.getDesiredVelocity(RPM), 0.0);
+    EXPECT_EQ(motor.getActualTorque(), 0.0);
+    EXPECT_EQ(motor.getDesiredTorque(), 0.0);
+    EXPECT_EQ(motor.getActualCurrent(), 0.0);
+    EXPECT_EQ(motor.getDesiredCurrent(), 0.0);
+    EXPECT_FALSE(motor.isTorqueMode());
+    EXPECT_TRUE(motor.isVelocityMode());
+}
+
+// Test case for constructor with max_rpm only
+TEST(MotorTest, ConstructorWithMaxRpmOnly) {
+    Motor motor(MAX_RPM);
+
+    EXPECT_EQ(motor.getActualVelocity(RPM), 0.0);
+    EXPECT_EQ(motor.getDesiredVelocity(RPM), 0.0);
+    EXPECT_EQ(motor.getActualTorque(), 0.0);
+    EXPECT_EQ(motor.getDesiredTorque(), 0.0);
+    EXPECT_EQ(motor.getActualCurrent(), 0.0);
+    EXPECT_EQ(motor.getDesiredCurrent(), 0.0);
+    EXPECT_TRUE(motor.isVelocityMode());
+    EXPECT_FALSE(motor.isTorqueMode());
+}
+
+// Test case for setting and getting actual velocity
+TEST(MotorTest, SetAndGetActualVelocity) {
+    Motor motor(MAX_RPM);
+    motor.setActualVelocity(100.0, RPM);
+    EXPECT_EQ(motor.getActualVelocity(RPM), 100.0);
+    // greater than max rpm, but it should not affect since it
+    // is the reading, not the setpoint
+    motor.setActualVelocity(10000.0, RPM);
+    EXPECT_EQ(motor.getActualVelocity(RPM), 10000.0);
+}
+
+// Test case for setting and getting desired velocity
+TEST(MotorTest, SetAndGetDesiredVelocity) {
+    Motor motor(MAX_RPM);
+    motor.setDesiredVelocity(200.0, RPM);
+    EXPECT_EQ(motor.getDesiredVelocity(RPM), 200.0);
+
+    motor.setDesiredVelocity(20000.0, RPM);
+    EXPECT_EQ(motor.getDesiredVelocity(RPM), MAX_RPM);
+}
+
+// Test case for setting and getting actual current
+TEST(MotorTest, SetAndGetActualCurrent) {
+    Motor motor(TORQUE, TORQUE_CONSTANT, MAX_RPM, MAX_AMP);
+    motor.setActualCurrent(2.0);
+    EXPECT_EQ(motor.getActualCurrent(), 2.0);
+}
+
+// Test case for setting and getting desired current
+TEST(MotorTest, SetAndGetDesiredCurrent) {
+    Motor motor(TORQUE, TORQUE_CONSTANT, MAX_RPM, MAX_AMP);
+    motor.setDesiredCurrent(1.0);
+    EXPECT_EQ(motor.getDesiredCurrent(), 1.0);
+    motor.setDesiredCurrent(MAX_AMP * 1.5);
+    EXPECT_EQ(motor.getDesiredCurrent(), MAX_AMP);
+}
+
+// Test case for setting and getting actual Torque
+TEST(MotorTest, SetAndGetActualTorque) {
+    Motor motor(TORQUE, TORQUE_CONSTANT, MAX_RPM, MAX_AMP);
+    motor.setActualTorque(0.02);
+    EXPECT_EQ(motor.getActualTorque(), 0.02);
+}
+
+// Test case for setting and getting desired Torque
+TEST(MotorTest, SetAndGetDesiredTorque) {
+    Motor motor(TORQUE, 2, MAX_RPM, MAX_AMP);
+    motor.setDesiredTorque(0.02);
+    EXPECT_EQ(motor.getDesiredTorque(), 0.02);
+}
