da7c1f571611698dc54605387b3b111f9192613c
Davide Dorigoni <ddorigoni@fbk.eu>
2024-04-08 15:29:46 +0200

    fix launch file package name

--- launch/maxxii.launch.py
+++ launch/maxxii.launch.py
@@ -8,14 +8,14 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
    config = os.path.join(
-      get_package_share_directory('maxxii_interface'),
+      get_package_share_directory('ros2_maxxii'),
       'config',
       'roboteq_driver.yaml'
       )
 
    return LaunchDescription([
       Node(
-        package='maxxii_interface',
+        package='ros2_maxxii',
         executable='maxxii_node',
         namespace='',
         name='maxxii_node',
